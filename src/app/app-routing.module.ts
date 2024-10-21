007b2963f1f01e3c5e3211b393017c07eecadfba
Diaa Yazbek <diaa.yazbek@wizme.com>
2024-10-21 17:34:34 +0300

    add-home-16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'test', component: HomeComponent},
+  { path: '',   redirectTo: '/test', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
